fa8bb7c51a10f9545b4261e3df73d78f09b61cf2
Ludvig Arnås <ludvig.arnas@gmail.com>
2020-04-08 14:48:22 +0200

    FilterAll

--- filterall/__init__.py
+++ filterall/__init__.py
@@ -1,6 +1,6 @@
-from .filter import Filter
+from .filterAll import FilterAll
 from redbot.core.bot import Red
 
 
 def setup(bot: Red):
-    bot.add_cog(Filter(bot))
+    bot.add_cog(FilterAll(bot))
